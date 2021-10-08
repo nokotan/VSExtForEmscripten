e31c8e6971995a3484306361a4b22ae1ccfcbdfb
kamenoko_nyokonyoko <kamenokonokotan@gmail.com>
2021-10-08 20:13:53 +0900

    Update

---
+++ Emscripten.Debugger.Definition/MyUnconfiguredProject.cs
@@ -0,0 +1,66 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+namespace Emscripten.Debugger.Definition
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+    using Microsoft.VisualStudio.ProjectSystem;
+    using Microsoft.VisualStudio.ProjectSystem.VS;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+    using Task = System.Threading.Tasks.Task;
+
+    [Export]
+    [AppliesTo(WasmDebugger.SchemaName)]
+    internal class MyUnconfiguredProject
+    {
+        /// <summary>
+        /// The file extension used by your project type.
+        /// This does not include the leading period.
+        /// </summary>
+        internal const string ProjectExtension = "wsproj";
+
+        internal const string Language = "WindowsScript";
+
+        [ImportingConstructor]
+        public MyUnconfiguredProject(UnconfiguredProject unconfiguredProject)
+        {
+            this.ProjectHierarchies = new OrderPrecedenceImportCollection<IVsHierarchy>(projectCapabilityCheckProvider: unconfiguredProject);
+        }
+
+        [Import]
+        internal UnconfiguredProject UnconfiguredProject { get; private set; }
+
+        [Import]
+        internal IActiveConfiguredProjectSubscriptionService SubscriptionService { get; private set; }
+
+        [Import]
+        internal IProjectThreadingService ProjectThreadingService { get; private set; }
+
+        [Import]
+        internal ActiveConfiguredProject<ConfiguredProject> ActiveConfiguredProject { get; private set; }
+
+        [Import]
+        internal ActiveConfiguredProject<MyConfiguredProject> MyActiveConfiguredProject { get; private set; }
+
+        [ImportMany(ExportContractNames.VsTypes.IVsProject, typeof(IVsProject))]
+        internal OrderPrecedenceImportCollection<IVsHierarchy> ProjectHierarchies { get; private set; }
+
+        internal IVsHierarchy ProjectHierarchy
+        {
+            get { return this.ProjectHierarchies.Single().Value; }
+        }
+    }
+}
