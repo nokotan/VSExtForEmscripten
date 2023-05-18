81e92affd0e4b48ddaaadec1e3ff878744f71c6b
nokotan <kamenokonokotan@gmail.com>
2023-05-19 03:33:13 +0900

    Add UnitTest

---
+++ Emscripten.Debugger.Definition.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Emscripten.Debugger.Definition.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Emscripten.Debugger.Definition.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3067b6a0-72bf-414f-b259-0838a12ffc55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Emscripten.Debugger.Definition.Test/WebAssemblyDebuggerConfig.test.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Emscripten.Debugger.Definition;
+using System;
+
+namespace Emscripten.Debugger.Definition.Test
+{
+    [TestClass]
+    public class WebAssemblyDebuggerConfigTest
+    {
+        [TestMethod]
+        public void GenerateChromeLaunchConfigFromEmptyStrings()
+        {
+            var config = WebAssemblyDebuggerConfig.GenerateChromeLaunchConfig(
+                inspectedPage: "",
+                chromeFlags: "",
+                chromeUserDataDirectory: "",
+                chromeIgnoreDefaultFlags: "",
+                debugAdapterExecutable: ""
+            );
+
+            Assert.AreEqual(config.flags.Length, 0);
+            Assert.AreEqual(config.userDataDir, null);
+            Assert.AreEqual(config.ignoreDefaultFlags, null);
+        }
+
+        [TestMethod]
+        public void GenerateChromeLaunchConfigFromValidFlags()
+        {
+            var config = WebAssemblyDebuggerConfig.GenerateChromeLaunchConfig(
+                inspectedPage: "",
+                chromeFlags: "--isolated;--mute;",
+                chromeUserDataDirectory: "",
+                chromeIgnoreDefaultFlags: "",
+                debugAdapterExecutable: ""
+            );
+
+            Assert.AreEqual(config.flags.Length, 2);
+            Assert.AreEqual(config.flags[0], "--isolated");
+            Assert.AreEqual(config.flags[1], "--mute");
+        }
+
+        [TestMethod]
+        public void GenerateChromeLaunchConfigFromValidUserDataDirectory()
+        {
+            var config = WebAssemblyDebuggerConfig.GenerateChromeLaunchConfig(
+                inspectedPage: "",
+                chromeFlags: "",
+                chromeUserDataDirectory: "/foo/bar",
+                chromeIgnoreDefaultFlags: "",
+                debugAdapterExecutable: ""
+            );
+
+            Assert.AreEqual(config.userDataDir, "/foo/bar");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void GenerateChromeLaunchConfigFromValidChromeIgnoreDefaultFlags()
+        {
+            var _config = WebAssemblyDebuggerConfig.GenerateChromeLaunchConfig(
+                inspectedPage: "",
+                chromeFlags: "",
+                chromeUserDataDirectory: "",
+                chromeIgnoreDefaultFlags: "yo",
+                debugAdapterExecutable: ""
+            );
+        }
+    }
+}
